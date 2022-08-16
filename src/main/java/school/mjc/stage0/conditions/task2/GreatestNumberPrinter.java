29719563e199dc5bb2fbdd727eb12fbcec9d4e16
timtechuz <timur.galiaskarov@timtech.uz>
2022-08-16 10:06:51 +0500

    stage0-module4-conditions-task2 first attempt

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+    	if (first > second) {
+    		System.out.println(first);
+    	} else if (first < second) {
+    		System.out.println(second);
+    	} else if (first == second) {
+    		System.out.println(first);
+    		System.out.println(second);
+    	}
     }
 }
